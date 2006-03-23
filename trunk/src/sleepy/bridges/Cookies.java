caf676a4b8f6b8abe188960e1a32c05ffefa2bef
oracel <oracel@02883b6b-fa0e-0410-8c30-ba3c2cce9b27>
2006-03-23 21:53:09 +0000

    Added Cookie bridge source skeleton

---
+++ trunk/src/sleepy/bridges/Cookies.java
@@ -0,0 +1,49 @@
+import sleep.bridges.*;
+import sleep.interfaces.*;
+import sleep.runtime.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+
+/**
+ * Cookie bridge for Sleep
+ * -------------------------------
+ * This is a generic cookie handling bridge for Sleep
+ * Designed to interface with Sleepy
+ *
+ * @author Andreas Ravnestad
+ * @since 1.0
+ */
+public class Cookies implements Loadable {
+
+    public boolean scriptLoaded(ScriptInstance s) {
+        return true;
+    }
+    public boolean scriptUnloaded(ScriptInstance s) {
+        return true;
+    }
+    
+    // getCookie(<name>)
+    // Returns a cookie value
+    private static class getCookie implements Function {
+        public Scalar evaluate(String name, ScriptInstance script, Stack args) {
+            return null;
+        }
+    }
+    
+    // setCookie(<name>, <value>, [duration], [domain])
+    // Sets a cookie
+    private static class setCookie implements Function {
+        public Scalar evaluate(String name, ScriptInstance script, Stack args) {
+            return null;
+        }
+    }
+    
+    // delCookie(<name>)
+    // Deletes a cookie entirely
+    private static class delCookie implements Function {
+        public Scalar evaluate(String name, ScriptInstance script, Stack args) {
+            return null;
+        }
+    }
+}
