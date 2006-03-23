d5bdd29f0025b04bf99247eb5b040815bbac90b6
oracel <oracel@02883b6b-fa0e-0410-8c30-ba3c2cce9b27>
2006-03-23 21:53:30 +0000

    Added Session bridge source skeleton

---
+++ trunk/src/sleepy/bridges/Sessions.java
@@ -0,0 +1,55 @@
+import sleep.bridges.*;
+import sleep.interfaces.*;
+import sleep.runtime.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+
+/**
+ * Session bridge for Sleep
+ * -------------------------------
+ * This is a bridge for manipulating HTTP sessions for Sleep
+ * Designed to interface with Sleepy
+ * Requires the Cookie handling bridge to be loaded
+ *
+ * @author Andreas Ravnestad
+ * @since 1.0
+ */
+public class Sessions implements Loadable {
+
+    public boolean scriptLoaded(ScriptInstance s) {
+        return true;
+    }
+    public boolean scriptUnloaded(ScriptInstance s) {
+        return true;
+    }
+    
+    // Starts a session
+    private static class startSession implements Function {
+        public Scalar evaluate(String name, ScriptInstance script, Stack args) {
+            return null;
+        }
+    }
+    
+    // Stops a session
+    private static class stopSession implements Function {
+        public Scalar evaluate(String name, ScriptInstance script, Stack args) {
+            return null;
+        }
+    }
+    
+    // Stops and destroys a session, deleting all session data associated
+    // with the given/current session
+    private static class destroySession implements Function {
+        public Scalar evaluate(String name, ScriptInstance script, Stack args) {
+            return null;
+        }
+    } 
+    
+    // Returns the current session id
+    private static class getSessionID implements Function {
+        public Scalar evaluate(String name, ScriptInstance script, Stack args) {
+            return null;
+        }
+    } 
+}
